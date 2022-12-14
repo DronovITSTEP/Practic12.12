bc444890ec2eb508b1dd9722a2bf3ef2527ecf87
ApxNord <dronov_a@t.top-academy.ru>
2022-12-14 19:24:05 +0300

    добавлены условия обслуживания следующего покупателя. условия для выхода из магазина. Подсчет общей выручки

--- Practic12.12/Practic12.12.cpp
+++ Practic12.12/Practic12.12.cpp
@@ -117,65 +117,117 @@ char i, k;
             price_petard = 19.99, price_chrisBox = 2.0,
             price_sweets = 5;
 
-        //меню магазина
-        cout << "1. Игрушки\t\t" << count_toys << " шт."<< "\t" 
-            << price_toys << "\n"
-            << "2. Елка\t\t\t " << count_trees << " шт." << "\t" 
-            << price_trees << "\n"
-            << "3. Петарды\t\t" << count_petard << " шт." << "\t" 
-            << price_petard << "\n"
-            << "4. Рождественская\n   упаковка\t\t" << count_crisBox 
-            << " шт." << "\t" << price_chrisBox << "\n"
-            << "5. Сладости\t\t" << count_sweets << " шт." << "\t" 
-            << price_sweets << "\n";
+        int x, c;
 
-        int i, count, allCount = 0;
-        cout << "Выбор товара -> ";
-        cin >> i;
-        switch (i)
-        {
-        case TOYS:
-            cout << "Кол-во -> ";
-            cin >> count;
-            allCount += count;
-            count_toys -= count;
-            cout << "Общая стоимость = " << price_toys * count << "$\n";
-            cout << "Добавить товар?(да/нет) ";
-            break;
-        case TREES:
-            cout << "Кол-во -> ";
-            cin >> count;
-            allCount += count;
-            count_trees -= count;
-            cout << "Общая стоимость = " << price_trees * count << "$\n";
-            cout << "Добавить товар?(да/нет) ";
-            break;
-        case PETARD:
-            cout << "Кол-во -> ";
-            cin >> count;
-            allCount += count;
-            count_petard -= count;
-            cout << "Общая стоимость = " << price_petard * count << "$\n";
-            cout << "Добавить товар?(да/нет) ";
-            break;
-        case CHRISTMAS_BOX:
-            cout << "Кол-во -> ";
-            cin >> count;
-            allCount += count;
-            count_crisBox -= count;
-            cout << "Общая стоимость = " << price_chrisBox * count << "$\n";
-            cout << "Добавить товар?(да/нет) ";
-            break;
-        case SWEETS:
-            cout << "Кол-во -> ";
-            cin >> count;
-            allCount += count;
-            count_sweets -= count;
-            cout << "Общая стоимость = " << price_sweets * count << "$\n";
-            cout << "Добавить товар?(да/нет) ";
-            break;
-        default:
-            break;
+        float sum_buyer, // сумма покупок покупаетля
+            sum_shop = 0;   // общая выручка магазина
+        while (1) {
+            sum_buyer = 0;
+            int allCount = 0;
+            do {
+                //меню магазина
+                cout << "Выручка магазина ==== " << sum_shop << "\n\n";
+                cout << "1. Игрушки\t\t" << count_toys << " шт." << "\t"
+                    << price_toys << "\n"
+                    << "2. Елка\t\t\t " << count_trees << " шт." << "\t"
+                    << price_trees << "\n"
+                    << "3. Петарды\t\t" << count_petard << " шт." << "\t"
+                    << price_petard << "\n"
+                    << "4. Рождественская\n   упаковка\t\t" << count_crisBox
+                    << " шт." << "\t" << price_chrisBox << "\n"
+                    << "5. Сладости\t\t" << count_sweets << " шт." << "\t"
+                    << price_sweets << "\n";
+
+                int i, count;
+                cout << "Выбор товара -> ";
+                cin >> i;
+                if (i >= 1 && i <= 5)
+                    switch (i)
+                    {
+                    case TOYS:
+                        do {
+                            cout << "Кол-во -> ";
+                            cin >> count;
+                            if (count > count_toys) cout << "У нас столько нет, берите меньше\n";
+                        } while (count > count_toys);
+                        allCount += count;
+                        count_toys -= count;
+                        cout << "Общая стоимость = " << price_toys * count << "$\n";
+                        sum_buyer += price_toys * count;
+                        break;
+                    case TREES:
+                        do {
+                            cout << "Кол-во -> ";
+                            cin >> count;
+                            if (count > count_trees) cout << "У нас столько нет, берите меньше\n";
+                        } while (count > count_trees);
+                        allCount += count;
+                        count_trees -= count;
+                        cout << "Общая стоимость = " << price_trees * count << "$\n";
+                        sum_buyer += price_trees * count;
+                        break;
+                    case PETARD:
+                        do {
+                            cout << "Кол-во -> ";
+                            cin >> count;
+                            if (count > count_petard) cout << "У нас столько нет, берите меньше\n";
+                        } while (count > count_petard);
+                        allCount += count;
+                        count_petard -= count;
+                        cout << "Общая стоимость = " << price_petard * count << "$\n";
+                        sum_buyer += price_petard * count;
+                        break;
+                    case CHRISTMAS_BOX:
+                        do {
+                            cout << "Кол-во -> ";
+                            cin >> count;
+                            if (count > count_crisBox) cout << "У нас столько нет, берите меньше\n";
+                        } while (count > count_crisBox);
+                        allCount += count;
+                        count_crisBox -= count;
+                        cout << "Общая стоимость = " << price_chrisBox * count << "$\n";
+                        sum_buyer += price_chrisBox * count;
+                        break;
+                    case SWEETS:
+                        do {
+                            cout << "Кол-во -> ";
+                            cin >> count;
+                            if (count > count_sweets) cout << "У нас столько нет, берите меньше\n";
+                        } while (count > count_sweets);
+                        allCount += count;
+                        count_sweets -= count;
+                        cout << "Общая стоимость = " << price_sweets * count << "$\n";
+                        sum_buyer += price_sweets * count;
+                        break;
+                    default:
+                        cout << "Неправильный выбор, у нас всего 5 наименований товара!\n";
+                        break;
+                    }
+                sum_shop += sum_buyer;
+                if (sum_buyer > 1500) {
+                    sum_buyer *= 0.9;
+                    cout << "Скидка на покупку составила 10%. Итого: " << sum_buyer << endl;
+                }
+
+
+                cout << "Выбрать еще товар? (1 - да/2 - нет): ";
+                cin >> x;
+                if (x == 2 && allCount < 4) {
+                    cout << "Нужно взять еще товара!\n";
+                    x = 0;
+                    system("pause");
+                    system("cls");
+
+                    continue;
+                }
+                system("cls");
+            } while (x != 2);
+
+            cout << "Еще работаем или пора закрывать магазин? (1 - да/ 2 - нет) -> ";
+            cin >> c;
+            if (c == 1) break;
         }
-        if (allCount < 4) cout << "Нужно взять еще товара!\n";
+
+        cout << "рабочий день закончен. Общая выручка магазина составила " << sum_shop << " зеленых";
+
 }
